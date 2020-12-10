5b827fa8a4bca6be2c5ee03f8d6b3ce350f1e789
kellsnc <kellspam@outlook.fr>
2020-12-10 23:52:44 +0100

    Rework AI system

--- sa2b-tails-ai/sa2b-tails-ai.cpp
+++ sa2b-tails-ai/sa2b-tails-ai.cpp
@@ -2,38 +2,47 @@
 
 Trampoline* LoadCharacters_t = nullptr;
 
-ObjectMaster* TailsAI_Ptr = nullptr;
-
-AnalogThing CopyAnalog[MaxPlayers];
-
-enum AIActions {
-	AI_Start,
-	AI_Unknown1,
-	AI_Respawn,
-	AI_Fly,
-	AI_Run,
-	AI_PlayerControl,
-	AI_Unknown6,
-	AI_Unknown7,
-	AI_Unknown8,
+enum class AIActions {
+	Start,
+	Unknown1,
+	Respawn,
+	Fly,
+	Run,
+	PlayerControl,
+	Unknown6,
+	Unknown7,
+	Unknown8,
 };
 
-enum AISubActions {
-	AISub_Init,
-	AISub_Normal,
-	AISub_Run,
-	AISub_Unknown3,
-	AISub_Unknown4,
-	AISub_Unknown5
+enum class AISubActions {
+	Init,
+	Normal,
+	Run,
+	Unknown3,
+	Unknown4,
+	Unknown5
 };
 
-void SetTailsFlying(EntityData1* twk, motionwk* pwk) {
+struct TailsAI {
+	AIActions action;
+	AISubActions subaction;
+	NJS_VECTOR pos_backup;
+	Rotation rot_backup;
+	int timer_rangeout;
+	int timer_nomove;
+};
+
+TailsAI* TailsAIWorker = nullptr;
+
+// AI functions
+
+void SetTailsFlying(EntityData1* twk, motionwk* pwk, CharObj2Base* co2) {
 	pwk->spd.z = 0.0;
 	pwk->spd.y = 0.0;
 	pwk->spd.x = 0.0;
-	pwk->work->Speed.z = 0.0;
-	pwk->work->Speed.y = 0.0;
-	pwk->work->Speed.x = 0.0;
+	co2->Speed.z = 0.0;
+	co2->Speed.y = 0.0;
+	co2->Speed.x = 0.0;
 	ForcePlayerAction(twk, GeneralAction_Fly);
 }
 
@@ -52,11 +61,10 @@ bool AI_RangeOut(EntityData1* p1, EntityData1* p2) {
 	return false;
 }
 
-void CharacterAI_WriteAnalog(EntityData1* data, EntityData1* playertwk, motionwk* playermwk, EntityData1* leadertwk) {
-	int playerid = data->Index;
-	int leaderid = data->field_2;
+void CharacterAI_WriteAnalog(TailsAI* aiwk, EntityData1* playertwk, motionwk* playermwk, CharObj2Base* playerco2, EntityData1* leadertwk) {
+	int playerid = playerco2->PlayerNum;
+	int leaderid = 0;
 
-	CharObj2Base* co2 = playermwk->work;
 	CharObj2Base* leadco2 = MainCharObj2[leaderid];
 
 	NJS_VECTOR* aipos = &playertwk->Position;
@@ -76,11 +84,11 @@ void CharacterAI_WriteAnalog(EntityData1* data, EntityData1* playertwk, motionwk
 	int pressed = 0;
 	int held = 0;
 
-	switch ((AISubActions)data->NextAction) {
-	case AISub_Init:
-		data->NextAction = AISub_Normal;
+	switch (aiwk->subaction) {
+	case AISubActions::Init:
+		aiwk->subaction = AISubActions::Normal;
 		break;
-	case AISub_Normal:
+	case AISubActions::Normal:
 
 		if (mag >= 15.0f) {
 			if (mag >= 40.0f) {
@@ -91,7 +99,7 @@ void CharacterAI_WriteAnalog(EntityData1* data, EntityData1* playertwk, motionwk
 			}
 		}
 		else if (leadco2->Speed.x >= (double)leadco2->PhysData.RollCancel /*|| co2->Speed.x <= (double)co2->PhysData.RollEnd*/) {
-			if (co2->Speed.x == 0.0f && mag >= 1.0f) {
+			if (playerco2->Speed.x == 0.0f && mag >= 1.0f) {
 				xmag = 0.0049999999f;
 			}
 			else {
@@ -107,180 +115,156 @@ void CharacterAI_WriteAnalog(EntityData1* data, EntityData1* playertwk, motionwk
 		if (playertwk->Status & (Status_Unknown1 | Status_Ground)) {
 			if (leadpos->y - aipos->y > 20.0f && leadco2->Speed.x < leadco2->PhysData.RollEnd) {
 				pressed |= JumpButtons;
-				//data->NextAction = AISub_Run;
+				//aiwk->subaction = AISubActions::Run;
 			}
 		}
 
 		break;
-	case AISub_Run:
+	case AISubActions::Run:
 		break;
 	default:
 		return;
 	}
 
-	if (GetDistance(&data->Position, aipos) >= 1.0f) {
-		data->Status = 0;
+	if (GetDistance(&aiwk->pos_backup, aipos) >= 1.0f) {
+		aiwk->timer_nomove = 0;
 	}
-
-	else if (++data->Status > 60 && mag > 30.0f)
-	{
-		data->Status = 0;
+	else if (++aiwk->timer_nomove > 60 && mag > 30.0f) {
+		aiwk->timer_nomove = 0;
 		pressed |= JumpButtons;
-		data->NextAction = AISub_Run;
+		aiwk->subaction = AISubActions::Run;
 	}
 
-	data->Position = *aipos;
-	data->Rotation = playertwk->Rotation;
+	aiwk->pos_backup = playertwk->Position;
+	aiwk->rot_backup = playertwk->Rotation;
 
 	Controllers[playerid].press |= pressed;
 	Controllers[playerid].on |= held;
 
 	AnalogThings[playerid].direction = angle;
 	AnalogThings[playerid].magnitude = xmag;
-
-	CopyAnalog[playerid] = AnalogThings[playerid];
 }
 
-void CharacterAI_Main(ObjectMaster* obj) {
-	EntityData1* data = obj->Data1.Entity;
+void TailsAI_Main(TailsAI* aiwk, EntityData1* playertwk, motionwk* playermwk, CharObj2Base* playerco2) {
+	int leaderid = 0; // Player the AI follows
+	int playerid = playerco2->PlayerNum;
 
-	// AI Player
-	EntityData1* playertwk = MainCharObj1[data->Index];
-	motionwk* playermwk = MainCharMotions[data->Index]; // contains movement, and CharObj2Base (phyiscs, anim...)
-	playermwk->work = MainCharObj2[data->Index];
-	int playerid = data->Index;
+	EntityData1* leadertwk = MainCharObj1[leaderid];
 
-	// Player the AI follows
-	EntityData1* leadertwk = MainCharObj1[data->field_2];
-	int leaderid = data->field_2;
-
-	switch ((AIActions)data->Action) {
-	case AI_Start: // Place the AI behind the leader
+	switch (aiwk->action) {
+	case AIActions::Start: // Place the AI behind the leader
 		DisableController(playerid);
 		GetPlayerSidePos(&playertwk->Position, leadertwk, 10.0);
-		data->Action = AI_Run;
-		data->NextAction = AISub_Init;
+		
+		aiwk->action = AIActions::Run;
+		aiwk->subaction = AISubActions::Init;
 		break;
-	case AI_Unknown1:
+	case AIActions::Unknown1:
 		playertwk->Position = leadertwk->Position;
 		playertwk->Position.y += 1000000.0f;
 		break;
-	case AI_Respawn: // Respawn after being too far
+	case AIActions::Respawn: // Respawn after being too far
 		DisableController(playerid);
 		SetToCameraPosition(&playertwk->Position);
 		playertwk->Position.y = leadertwk->Position.y + 50.0f;
-		SetTailsFlying(playertwk, playermwk);
-		Controllers[playerid].on |= Buttons_A;
-		Controllers[playerid].press |= Buttons_A; 
-		data->NextAction = AISub_Unknown3;
-		data->Action = AI_Run;
-		break;
-	case AI_Fly:
-		DisableController(playerid);
-		SetTailsFlying(playertwk, playermwk);
+		SetTailsFlying(playertwk, playermwk, playerco2);
 		Controllers[playerid].on |= Buttons_A;
 		Controllers[playerid].press |= Buttons_A;
-		data->NextAction = AISub_Unknown3;
+		
+		aiwk->action = AIActions::Run;
+		aiwk->subaction = AISubActions::Unknown3;
 		break;
-	case AI_Run: // Run the main movements
+	case AIActions::Fly:
+		DisableController(playerid);
+		SetTailsFlying(playertwk, playermwk, playerco2);
+		Controllers[playerid].on |= Buttons_A;
+		Controllers[playerid].press |= Buttons_A;
+
+		aiwk->subaction = AISubActions::Unknown3;
+		break;
+	case AIActions::Run: // Run the main movements
 
 		// If the player controller has input, deactive tails AI temporarily
 		if (Controllers[playerid].on & (Buttons_L | Buttons_R | Buttons_X | Buttons_Y | Buttons_Right | Buttons_Left | Buttons_Down | Buttons_Up | Buttons_A | Buttons_B)) {
 			EnableController(playerid);
-			data->field_6 = 0;
-			data->Action = AI_PlayerControl;
+			aiwk->timer_rangeout = 0;
+			aiwk->action = AIActions::PlayerControl;
 		}
 		else {
 			DisableController(playerid);
 		}
-		
-		CharacterAI_WriteAnalog(data, playertwk, playermwk, leadertwk);
-		
+
+		CharacterAI_WriteAnalog(aiwk, playertwk, playermwk, playerco2, leadertwk);
+
 		// Too far away
 		if (AI_RangeOut(playertwk, leadertwk)) {
-			data->Action = AI_Respawn;
+			aiwk->action = AIActions::Respawn;
 		}
 
 		break;
-	case AI_PlayerControl: // let the player be moved by controller, reactive AI if 300 frames elapsed
+	case AIActions::PlayerControl: // let the player be moved by controller, reactive AI if 300 frames elapsed
 		if (Controllers[playerid].on & (Buttons_L | Buttons_R | Buttons_X | Buttons_Y | Buttons_Right | Buttons_Left | Buttons_Down | Buttons_Up | Buttons_A | Buttons_B)) {
 			EnableController(playerid);
-			data->field_6 = 0;
+			aiwk->timer_rangeout = 0;
 		}
 		else {
-			if (++data->field_6 > 300) {
+			if (++aiwk->timer_rangeout > 300) {
 				DisableController(playerid);
-				data->field_6 = 0;
-				data->Action = AI_Run;
+				aiwk->timer_rangeout = 0;
+				aiwk->action = AIActions::Run;
 			}
 		}
 
 		// Too far away
 		if (AI_RangeOut(playertwk, leadertwk)) {
-			data->field_6 = 0;
-			data->Action = AI_Respawn;
+			aiwk->timer_rangeout = 0;
+			aiwk->action = AIActions::Respawn;
 		}
 
 		break;
-	case AI_Unknown6:
+	case AIActions::Unknown6:
 		break;
-	case AI_Unknown7:
+	case AIActions::Unknown7:
 		break;
-	case AI_Unknown8:
+	case AIActions::Unknown8:
 		break;
 	default:
 		return;
 	}
 }
 
-void CharacterAI_Delete(ObjectMaster* obj) {
-	TailsAI_Ptr = nullptr;
+// Tails' Object with the AI:
+
+void __cdecl TailsWithAI_Main(ObjectMaster* obj) {
+	TailsAI_Main((TailsAI*)obj->field_4C, obj->Data1.Entity, (motionwk*)obj->EntityData2, obj->Data2.Character);
+	Tails_Main(obj);
 }
 
-void LoadCharacterAI(int player, int leader) {
-	ObjectMaster* aitask = LoadObject(1, "CharacterAI", CharacterAI_Main, LoadObj_Data1);
-	EntityData1* data = aitask->Data1.Entity;
+void __cdecl TailsWithAI_Delete(ObjectMaster* obj) {
+	delete TailsAIWorker;
+}
 
-	aitask->DeleteSub = CharacterAI_Delete;
+void LoadTailsWithAI(int playerid) {
+	LoadTails(1);
 
-	data->Index = player;
-	data->field_2 = leader;
-	data->Action = AI_Start;
+	TailsAIWorker = new TailsAI();
 
-	TailsAI_Ptr = aitask;
+	MainCharacter[playerid]->field_4C = TailsAIWorker;
+	MainCharacter[playerid]->MainSub = TailsWithAI_Main;
+	MainCharacter[playerid]->DeleteSub = TailsWithAI_Delete;
 }
 
 void LoadCharacters_r() {
 	NonStaticFunctionPointer(void, Original, (), LoadCharacters_t->Target());
 
+	// Load Tails AI if in a Sonic level
 	if (TwoPlayerMode == false) {
 		if (CurrentCharacter == Characters_Sonic) {
-			LoadTails(1); // If you load tails first, no animation issue somehow
-			InitPlayer(1);
-			LoadCharacterAI(1, 0);
-			WriteData<1>((char*)0x46B02E, (char)0x01); // Deathzone only for P1
+			LoadTailsWithAI(1);
 		}
-	}
-	else {
-		WriteData<1>((char*)0x46B02E, (char)0x02); // Restore DeathZone
 	}
 
 	Original();
-}
-
-// Temporary trampoline because this game sucks
-void __cdecl Tails_r(ObjectMaster* obj);
-Trampoline Tails_t((int)Tails_Main, (int)Tails_Main + 0x6, Tails_r);
-void __cdecl Tails_r(ObjectMaster* obj) {
-	ObjectFunc(orig, Tails_t.Target());
-
-	if (CopyAnalog[1].direction) {
-		AnalogThings[1].magnitude = CopyAnalog[1].magnitude;
-		AnalogThings[1].direction = CopyAnalog[1].direction;
-	}
-	
-	orig(obj);
-	TwoPlayerMode = 0;
 }
 
 extern "C" {
